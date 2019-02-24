7173e9287a2561fc588fda1250eb5821328ccde8
RKJO <rkjozwiak@gmail.com>
2019-02-24 14:23:31 +0100

    try to create sothing

--- simple_app/views.py
+++ simple_app/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rdflib import Graph, URIRef, RDF
+uri = URIRef('http://dbpedia.org/resource/Kazik_Staszewski')
+person = URIRef('http://dbpedia.org/ontology/Person')
+
+g = Graph()
+g.parse(uri)
+print(g.parse(uri))
+
+# for obj in g.objects(subject=uri, predicate=RDF.type):
+#     if obj == person:
+#         print(uri, "is a", person, obj)
