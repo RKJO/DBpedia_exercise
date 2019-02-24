0ee40f21d03b7be6dfd9299dc663adca43466111
RKJO <rkjozwiak@gmail.com>
2019-02-24 13:26:15 +0100

    fix

--- db_exp/settings.py
+++ db_exp/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'simple_app.apps.Simpele_appConfig',
+    'simple_app',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
